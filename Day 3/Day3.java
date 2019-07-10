f84225c091cc1907680553eb38218c380ae916dc
Ishitak1996 <ishitakumar712@gmail.com>
2019-07-10 15:44:50 +0530

    Day 3

---
+++ Day 3/Day3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Integer> map = new HashMap<String,Integer>();
+
+
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            map.put(name,phone);
+        }
+        while(in.hasNext()){
+            String name = in.next();
+            if (map.containsKey(name))
+            {
+                System.out.println(name+"="+map.get(name));
+            }
+            else 
+            {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
+
