0af39baf14b889852c5d120d95adae68beb47ba4
Ishitak1996 <ishitakumar712@gmail.com>
2019-07-13 14:56:16 +0530

    Day 5

---
+++ Day 5/Day 5.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+  public class Day9Recursion {
+    public static int factorial(int i) {
+        if (i == 1 || i == 0) {
+            return 1;
+        } else {
+            return i * factorial(i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(factorial(i));
+        sc.close();
+    }
+}
